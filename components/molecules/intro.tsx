903ae9e9666b8dad6694bd2c5fe0f3fb52feccde
Tony Ko <tony@tko.dev>
2026-05-12 17:19:58 -0400

    style(home): refine intro layout, avatar blur, and card-home typography

--- components/molecules/intro.tsx
+++ components/molecules/intro.tsx
@@ -6,34 +6,29 @@ import { cn, cva, VariantProps } from '@/utils/theme'
 
 const styles = {
   root: cva([
-    'mb-30 aspect-square h-auto w-full md:w-2/3 lg:mb-0 lg:w-3/5',
+    'mb-30 h-auto w-full md:mb-25 md:w-3/4 lg:mb-0 lg:w-4/5',
     'relative',
     'flex flex-col items-center justify-center'
   ]),
   cross: cva(['centered absolute']),
   brand1: cva([
-    'relative -left-8 h-auto w-[75%]',
+    'relative -left-8 h-auto w-3/4',
     'animate-slide-down transition-all duration-1000 hover:scale-105'
   ]),
   brand2: cva([
-    'relative h-auto w-[75%]',
+    'relative h-auto w-3/4',
     'animate-fade-in transition-all duration-1000 hover:scale-105'
   ]),
   brand3: cva([
-    'relative left-8 h-auto w-[75%]',
+    'relative left-8 h-auto w-3/4',
     'animate-slide-up transition-all duration-1000 hover:scale-105'
   ]),
   avatar: cva([
-    'aspect-square h-auto w-[50%]',
+    'aspect-square h-auto w-3/5 opacity-75',
     'centered absolute',
-    'grayscale-25 transition-all duration-1000 hover:scale-105 hover:grayscale'
+    'blur-3xl grayscale-25 transition-opacity duration-1000 hover:opacity-100'
   ]),
-  body: cva([
-    'absolute h-auto w-60',
-    'top-[80%] left-1/2 -translate-x-1/2 text-center',
-    'lg:top-1/2 lg:left-full lg:-translate-1/2 lg:text-left',
-    'flex flex-col gap-2'
-  ])
+  body: cva(['centered absolute h-auto w-70 text-center', 'flex flex-col gap-2'])
 }
 
 type IntroRef = HTMLDivElement
@@ -45,7 +40,7 @@ const Intro = forwardRef<IntroRef, IntroProps>((props, ref) => {
   return (
     <div ref={ref} className={cn(styles.root({ className }))} {...rest}>
       <Cross className={cn(styles.cross())} />
-      <Brand className={cn(styles.brand1())} variant="fill" />
+      <Brand className={cn(styles.brand1())} variant="outline" />
       <Brand className={cn(styles.brand2())} variant="outline" />
       <Avatar className={cn(styles.avatar())}>
         <AvatarImage alt="Tony Ko" src="/images/tkodev/dp-sq.jpg" />
